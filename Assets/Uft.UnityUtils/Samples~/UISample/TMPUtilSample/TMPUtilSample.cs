9c26df03989610eb7919f997a7a4077129941603
HOME-20230701\Ether <etherealgarden@msn.com>
2025-04-06 21:09:54 +0900

    Sample追加

---
+++ Assets/Uft.UnityUtils/Samples~/UISample/TMPUtilSample/TMPUtilSample.cs
@@ -0,0 +1,29 @@
+﻿using Uft.UnityUtils.UI;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Uft.UnityUtils.Samples
+{
+    public class TMPUtilSample : MonoBehaviour
+    {
+        [SerializeField] Button _button;
+        [SerializeField] TMP_Text _tmpText;
+        [SerializeField] ScrollRect _scrollRect;
+
+        void Start()
+        {
+            _button.onClick.AddListener(UniTask.UnityAction(async () =>
+            {
+                _tmpText.text = "";
+                await _tmpText.TypeWriterEffectAsync(@"O, that this too too solid flesh would melt,
+Thaw, and resolve itself into a dew!
+Or that the Everlasting had not fix'd
+His canon 'gainst self-slaughter! O God! God!
+How weary, stale, flat, and profitable
+Seem to me all the uses of this world!", TMPUtil.ONE_FRAME * 2, default, _scrollRect);
+            }));
+        }
+    }
+}
