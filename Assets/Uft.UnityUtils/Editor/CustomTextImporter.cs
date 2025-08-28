72a329ffb54cc52d4cd7184d8ceb90eb3ed0d036
HOME-20230701\Ether <etherealgarden@msn.com>
2025-08-28 22:34:35 +0900

    ScriptedImporter追加

---
+++ Assets/Uft.UnityUtils/Editor/CustomTextImporter.cs
@@ -0,0 +1,20 @@
+﻿#if !DISABLE_UNITY_UTILS_SCRIPTED_IMPORTER
+using System.IO;
+using UnityEditor.AssetImporters;
+using UnityEngine;
+
+namespace Uft.UnityUtils.Editor
+{
+    [ScriptedImporter(1, new[] { "jsonl", "toml" })]
+    public class CustomTextImporter : ScriptedImporter
+    {
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            string content = File.ReadAllText(ctx.assetPath);
+            var textAsset = new TextAsset(content);
+            ctx.AddObjectToAsset("text", textAsset);
+            ctx.SetMainObject(textAsset);
+        }
+    }
+}
+#endif
