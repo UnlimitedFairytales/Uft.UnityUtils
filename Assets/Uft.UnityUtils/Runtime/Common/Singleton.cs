7f71cd4c90fd564fcaf949149a61b101dede97d3
HOME-20230701\Ether <etherealgarden@msn.com>
2025-09-13 15:50:19 +0900

    Singleton追加

---
+++ Assets/Uft.UnityUtils/Runtime/Common/Singleton.cs
@@ -0,0 +1,20 @@
+﻿namespace Assets.Uft.UnityUtils.Runtime.Common
+{
+    public class Singleton<T> where T : new()
+    {
+        static readonly object _lock = new();
+        static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    _instance ??= new();
+                }
+                return _instance;
+            }
+        }
+    }
+}
