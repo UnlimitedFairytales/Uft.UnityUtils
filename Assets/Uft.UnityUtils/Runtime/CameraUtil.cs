0e54cea389bb520ddc77fc53ef3881c444bdf7b5
SNOW\Ether <etherealgarden@msn.com>
2025-12-15 06:03:24 +0900

    微調整

--- Assets/Uft.UnityUtils/Runtime/CameraUtil.cs
+++ Assets/Uft.UnityUtils/Runtime/CameraUtil.cs
@@ -14,7 +14,7 @@ namespace Uft.UnityUtils
         {
             return h / (2 * Mathf.Tan(vFov / 2 * Mathf.Deg2Rad));
         }
-        public const float DISTANCE_720P_FOV60 = 623.538290725f;
-        public const float DISTANCE1080P_FOV60 = 935.307436087f;
+        public const float DISTANCE_720P_FOV60 = 623.5383f; // 623.538290725f;
+        public const float DISTANCE1080P_FOV60 = 935.3075f; // 935.307436087f;
     }
 }
