0fe055a44a8a95d7eb4835e26e54ec6187c3cf35
SNOW\Ether <etherealgarden@msn.com>
2025-12-17 05:05:29 +0900

    this追記

--- Assets/Uft.UnityUtils/Samples~/UISample/MessageBoxSample/MessageBoxSample.cs
+++ Assets/Uft.UnityUtils/Samples~/UISample/MessageBoxSample/MessageBoxSample.cs
@@ -1,4 +1,4 @@
-﻿using Uft.UnityUtils.UI;
+using Uft.UnityUtils.UI;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,11 +15,11 @@ namespace Uft.UnityUtils.Samples.UISample
 
         void Start()
         {
-            _toastUI = ComponentUtil.Instantiate(_toastUI_prefab, transform, false, false);
-            _button.onClick.AddListener(UniTask.UnityAction(async () =>
+            this._toastUI = ComponentUtil.Instantiate(this._toastUI_prefab, this.transform, false, false);
+            this._button.onClick.AddListener(UniTask.UnityAction(async () =>
             {
-                await _toastUI.ShowAsync("Header text", "This is content text.", 5);
-                Debug.Log($"{nameof(_toastUI.ShowAsync)} complete");
+                await this._toastUI.ShowAsync("Header text", "This is content text.", 5);
+                Debug.Log($"{nameof(this._toastUI.ShowAsync)} complete");
             }));
         }
     }
