058e5b47a34768f79671d8cd46ef2661c7923c7e
SNOW\Ether <etherealgarden@msn.com>
2025-12-17 05:58:11 +0900

    this対応

--- Assets/Uft.UnityUtils/Samples~/UISample/TMPUtilSample/TMPUtilSample.cs
+++ Assets/Uft.UnityUtils/Samples~/UISample/TMPUtilSample/TMPUtilSample.cs
@@ -1,4 +1,4 @@
-﻿using Uft.UnityUtils.UI;
+using Uft.UnityUtils.UI;
 using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
@@ -14,15 +14,15 @@ namespace Uft.UnityUtils.Samples.UISample
 
         void Start()
         {
-            _button.onClick.AddListener(UniTask.UnityAction(async () =>
+            this._button.onClick.AddListener(UniTask.UnityAction(async () =>
             {
-                _tmpText.text = "";
-                await _tmpText.TypeWriterEffectAsync(@"O, that this too too solid flesh would melt,
+                this._tmpText.text = "";
+                await this._tmpText.TypeWriterEffectAsync(@"O, that this too too solid flesh would melt,
 Thaw, and resolve itself into a dew!
 Or that the Everlasting had not fix'd
 His canon 'gainst self-slaughter! O God! God!
 How weary, stale, flat, and profitable
-Seem to me all the uses of this world!", TMPUtil.ONE_FRAME * 2, default, _scrollRect);
+Seem to me all the uses of this world!", TMPUtil.ONE_FRAME * 2, default, this._scrollRect);
             }));
         }
     }
