9febe57272da7a04296cc4832a623af87e9906a3
SNOW\Ether <etherealgarden@msn.com>
2025-12-20 00:27:59 +0900

    微調整

--- Assets/Uft.UnityUtils/Runtime/ComponentUtil.cs
+++ Assets/Uft.UnityUtils/Runtime/ComponentUtil.cs
@@ -1,4 +1,6 @@
-﻿using System;
+#nullable enable
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,7 +9,7 @@ namespace Uft.UnityUtils
 {
     public static class ComponentUtil
     {
-        public static TComponent Instantiate<TComponent>(this TComponent original, Transform parent = null, bool worldPositionStays = true, bool? isActive = null)
+        public static TComponent Instantiate<TComponent>(this TComponent original, Transform? parent = null, bool worldPositionStays = true, bool? isActive = null)
             where TComponent : Component
         {
             var instantiated = UnityEngine.Object.Instantiate(original);
@@ -16,26 +18,26 @@ namespace Uft.UnityUtils
             return instantiated;
         }
 
-        public static List<TComponent> GetComponentsInChildrenOrderByName<TComponent>(this Component component, Func<TComponent, bool> whereCondition = null) where TComponent : Component
+        public static List<TComponent> GetComponentsInChildrenOrderByName<TComponent>(this Component component, bool includeInactive, Func<TComponent, bool>? whereCondition = null) where TComponent : Component
         {
             if (whereCondition == null)
             {
-                return component.GetComponentsInChildren<TComponent>()
+                return component.GetComponentsInChildren<TComponent>(includeInactive)
                     .OrderBy(c => c.gameObject.name)
                     .ToList();
             }
             else
             {
-                return component.GetComponentsInChildren<TComponent>()
+                return component.GetComponentsInChildren<TComponent>(includeInactive)
                     .Where(whereCondition)
                     .OrderBy(c => c.gameObject.name)
                     .ToList();
             }
         }
 
-        public static List<TComponent> GetComponentsInChildrenByName<TComponent>(this Component component, string name) where TComponent : Component
+        public static List<TComponent> GetComponentsInChildrenByName<TComponent>(this Component component, bool includeInactive, string name) where TComponent : Component
         {
-            return component.GetComponentsInChildren<TComponent>()
+            return component.GetComponentsInChildren<TComponent>(includeInactive)
                 .Where(c => c.gameObject.name == name)
                 .ToList();
         }
