19313222a46795413b0ebb211b0cb162586050a4
SNOW\Ether <etherealgarden@msn.com>
2025-12-21 02:28:24 +0900

    微調整

--- Assets/Uft.UnityUtils/Runtime/ParticleSystemUtil.cs
+++ Assets/Uft.UnityUtils/Runtime/ParticleSystemUtil.cs
@@ -1,4 +1,6 @@
-﻿using Cysharp.Threading.Tasks;
+#nullable enable
+
+using Cysharp.Threading.Tasks;
 using System.Threading;
 using UnityEngine;
 
@@ -6,6 +8,7 @@ namespace Uft.UnityUtils
 {
     public static class ParticleSystemUtil
     {
+        /// <summary>subEmittersは1段目のみ対応で、再帰的にチェックしていません。また、loop=trueな内容はDelay判定から無視されます。</summary>
         public static async UniTask DelayForParticleSystem(this GameObject instantiated, CancellationToken cancellationToken = default)
         {
             var effects = instantiated.GetComponentsInChildren<ParticleSystem>(true);
@@ -13,30 +16,51 @@ namespace Uft.UnityUtils
             while (isAnyPlaying)
             {
                 isAnyPlaying = false;
-                foreach (var effect in effects)
+                for (int i = 0; i < effects.Length; i++)
                 {
-                    if (effect.isPlaying)
+                    var effect = effects[i];
+                    if (!effect) continue;
+                    if (effect.gameObject.activeInHierarchy && effect.isPlaying && !effect.main.loop)
                     {
                         isAnyPlaying = true;
                         break;
                     }
+                    var subs = effect.subEmitters;
+                    for (int j = 0; j < subs.subEmittersCount; j++)
+                    {
+                        var sub = subs.GetSubEmitterSystem(j);
+                        if (!sub) continue;
+                        if (sub.gameObject.activeInHierarchy && sub.isPlaying && !sub.main.loop)
+                        {
+                            isAnyPlaying = true;
+                            break;
+                        }
+                    }
+                    if (isAnyPlaying) break;
                 }
                 await UniTask.NextFrame(cancellationToken);
             }
         }
 
-        public static async UniTask CreateAndPlayEffectAsync(this GameObject effect_prefab, Vector3 screenPos, float distance, Camera camera, Transform parent, CancellationToken cancellationToken = default)
+        /// <summary>subEmittersが生成元の子にならない場合、Destroy対象にならないため注意してください。生成後のawaitはDelayForParticleSystemに従います。</summary>
+        public static async UniTask CreateAndPlayEffectAsync(this GameObject effectPrefab, Vector3 screenPos, float distance, Camera camera, Transform parent, CancellationToken cancellationToken = default)
         {
-            var instantiated = Object.Instantiate(effect_prefab, parent);
-            if (instantiated != null)
+            var instantiated = Object.Instantiate(effectPrefab, parent);
+            if (instantiated)
             {
-                screenPos.z = distance;
-                var pos3D = camera.ScreenToWorldPoint(screenPos);
-                instantiated.transform.position = pos3D;
-                await DelayForParticleSystem(instantiated, cancellationToken);
-                if (instantiated != null)
+                try
                 {
-                    Object.Destroy(instantiated);
+                    screenPos.z = distance;
+                    var pos3D = camera.ScreenToWorldPoint(screenPos);
+                    instantiated.transform.position = pos3D;
+                    await DelayForParticleSystem(instantiated, cancellationToken);
+                }
+                finally
+                {
+                    if (instantiated)
+                    {
+                        Object.Destroy(instantiated);
+                    }
                 }
             }
         }
