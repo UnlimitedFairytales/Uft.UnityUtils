0df097fa580f2960a240c7681f6b423ccd4367d4
SNOW\Ether <etherealgarden@msn.com>
2025-12-21 03:47:00 +0900

    微調整

--- Assets/Uft.UnityUtils/Samples~/PlayerLoopSample/PlayerLoopSample.cs
+++ Assets/Uft.UnityUtils/Samples~/PlayerLoopSample/PlayerLoopSample.cs
@@ -1,12 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Uft.UnityUtils.Samples.PlayerLoopSample
 {
     public class PlayerLoopSample : MonoBehaviour
     {
+        const string NAME = "[" + nameof(PlayerLoopSample) + "]";
+
         void Start()
         {
+            DevLog.Log($"{NAME} Start()");
             SceneManager.LoadScene("PlayerLoopSample_Additive", LoadSceneMode.Additive);
         }
     }
