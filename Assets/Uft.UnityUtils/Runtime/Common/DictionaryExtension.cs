3eb1d716dc6c37f3534fa6e6e14c70bc338918f2
SNOW\Ether <etherealgarden@msn.com>
2025-12-21 17:23:25 +0900

    微調整

--- Assets/Uft.UnityUtils/Runtime/Common/DictionaryExtension.cs
+++ Assets/Uft.UnityUtils/Runtime/Common/DictionaryExtension.cs
@@ -1,4 +1,5 @@
-﻿using System;
+#nullable enable
+
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -7,16 +8,13 @@ namespace Uft.UnityUtils.Common
 {
     public static class DictionaryExtension
     {
-        public static Dictionary<TKey, TVal> ToDictionary<TKey, TVal>(this List<TKey> keys, List<TVal> vals)
+        /// <summary>valsが足らない場合、default(T)で埋めます（参照型は null、値型は既定値）</summary>
+        public static Dictionary<TKey, TVal?> ToDictionary<TKey, TVal>(this List<TKey> keys, List<TVal> vals)
         {
-            if (keys == null) return new Dictionary<TKey, TVal>();
-
-            var dic = new Dictionary<TKey, TVal>();
+            var dic = new Dictionary<TKey, TVal?>();
             for (int i = 0; i < keys.Count; i++)
             {
-                var val = vals != null && i < vals.Count ?
-                    vals[i] :
-                    default;
+                var val = i < vals.Count ? vals[i] : default;
                 dic.Add(keys[i], val);
             }
             return dic;
@@ -24,8 +22,6 @@ namespace Uft.UnityUtils.Common
 
         public static KeyValuePair<List<TKey>, List<TVal>> ToListPair<TKey, TVal>(this IDictionary<TKey, TVal> dic)
         {
-            if (dic == null) return new KeyValuePair<List<TKey>, List<TVal>>(new List<TKey>(), new List<TVal>());
-
             var keys = dic.Keys.ToList();
             var vals = dic.Values.ToList();
             return new KeyValuePair<List<TKey>, List<TVal>>(keys, vals);
@@ -36,13 +32,9 @@ namespace Uft.UnityUtils.Common
             dic[key] = val;
         }
 
-        public static bool ContainsAndEquals<TKey, TVal>(this Dictionary<TKey, TVal> dic, TKey key, TVal val) where TVal : IEquatable<TVal>
+        public static bool ContainsAndEquals<TKey, TVal>(this Dictionary<TKey, TVal> dic, TKey key, TVal val)
         {
-            if (dic.ContainsKey(key))
-            {
-                return dic[key].Equals(val);
-            }
-            return false;
+            return dic.TryGetValue(key, out var v) && EqualityComparer<TVal>.Default.Equals(v, val);
         }
 
         public static ReadOnlyDictionary<TKey, TVal> CloneAsReadOnly<TKey, TVal>(this IDictionary<TKey, TVal> dictionary)
