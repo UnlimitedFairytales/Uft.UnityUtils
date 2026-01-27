053e8604fd5a6210dc77385fe103bcdd08f21a51
SNOW\Ether <etherealgarden@msn.com>
2026-01-28 01:40:52 +0900

    WorldToLocalPointInRect追加

--- Assets/Uft.UnityUtils/Runtime/UI/RectTransformUtil.cs
+++ Assets/Uft.UnityUtils/Runtime/UI/RectTransformUtil.cs
@@ -41,5 +41,36 @@ namespace Uft.UnityUtils.UI
             }
             return canvasRect.TransformPoint(canvasLocal);
         }
+
+        /// <summary>
+        /// World座標をRectTransformのLocal座標に変換する。
+        /// </summary>
+        /// <remarks>
+        /// - World -> Screen -> RectTransformLocalと2段階変換して求めている。<br/>
+        /// - CanvasScalerも自動的に考慮される。<br/>
+        /// - 典型的な用途では、targetRectのPivot位置と計算結果を使用する対象のPivot位置の実座標を同一にしておく。(このようにしておくと、単にanchoredPositionに計算結果を渡せば良い。)<br/>
+        /// </remarks>
+        /// <param name="worldCamera">World -> Screen のためのカメラ</param>
+        /// <param name="worldPos">元のWorld座標</param>
+        /// <param name="canvas">Screen -> RectTransformLocal のための Canvas</param>
+        /// <param name="targetRect">Screen -> RectTransformLocal のための rect。通常は、座標設定したいノードの親ノードのRectTransformを与える。</param>
+        /// <param name="localPoint">tagetRectのPivot位置からのRect相対座標。通常は、親(targetRect)と子(座標指定したいノード)のPivot位置を同一座標になるようにしておく。この場合なら、そのままanchoredPositionに渡せる値になる。</param>
+        /// <returns></returns>
+        public static bool WorldToLocalPointInRect(this Camera worldCamera, Vector3 worldPos, Canvas canvas, RectTransform targetRect, out Vector2 localPoint)
+        {
+            localPoint = Vector2.zero;
+
+            // 1. world -> screen
+            Vector3 screen = worldCamera.WorldToScreenPoint(worldPos);
+            if (screen.z < 0f) return false;
+
+            // 2. screen -> rectLocal
+            Camera? canvasCamera = canvas.renderMode != RenderMode.ScreenSpaceOverlay ? canvas.worldCamera : null;
+            return RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                targetRect,
+                screen,
+                canvasCamera,
+                out localPoint);
+        }
     }
 }
---
+++ Assets/Uft.UnityUtils/Samples~/UISample2/RectTransformUtilSample.cs
@@ -0,0 +1,24 @@
+using Uft.UnityUtils.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Uft.UnityUtils.Samples.UISample
+{
+    public class RectTransformUtilSample : MonoBehaviour
+    {
+        [SerializeField] GameObject[] _cubes;
+        [SerializeField] Image[] _images;
+
+        void Start()
+        {
+            var canvas = this._images[0].canvas;
+            for (int i = 0; i < this._cubes.Length; i++)
+            {
+                var worldTransform = this._cubes[i].transform;
+                var parent = this._images[i].rectTransform;
+                RectTransformUtil.WorldToLocalPointInRect(Camera.main, worldTransform.position, canvas, parent, out Vector2 resultPoint);
+                this._images[i].rectTransform.anchoredPosition = resultPoint;
+            }
+        }
+    }
+}
