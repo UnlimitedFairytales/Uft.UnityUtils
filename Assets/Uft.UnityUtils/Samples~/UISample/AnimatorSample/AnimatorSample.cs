d6f1da07274fc5f0e3e998436c12c2c579e9c7ab
SNOW\Ether <etherealgarden@msn.com>
2026-02-07 15:45:48 +0900

    移動と名前の調整

--- Assets/Uft.UnityUtils/Samples~/UISample/AnimatorSample/AnimatorSample.cs
+++ Assets/Uft.UnityUtils/Samples~/UISample/AnimatorSample/AnimatorSample.cs
@@ -9,8 +9,8 @@ namespace Uft.UnityUtils.Samples.UISample
 #pragma warning disable IDE0051 // 使用されていないプライベート メンバーを削除する
         const string SHOWING = "Showing";
         const string SHOWN = "Shown";
-        const string CLOSING = "Closing";
-        const string CLOSED = "Closed";
+        const string HIDIING = "Hiding";
+        const string HIDDEN = "Hidden";
 #pragma warning restore IDE0051 // 使用されていないプライベート メンバーを削除する
 
         [SerializeField] Button _button;
@@ -22,7 +22,7 @@ namespace Uft.UnityUtils.Samples.UISample
         {
             this._button.onClick.AddListener(UniTask.UnityAction(async () =>
             {
-                var nextStateName = this._isClosed ? SHOWING : CLOSING;
+                var nextStateName = this._isClosed ? SHOWING : HIDIING;
                 this._isClosed = !this._isClosed;
                 this._animator.SetTrigger(nextStateName);
                 await this._animator.DelayForAnimation(nextStateName, true);
