45e7162e83b7081c60e9165a2ae16472dcd83194
SNOW\Ether <etherealgarden@msn.com>
2026-04-23 00:32:01 +0900

    リファクタ

--- Assets/Uft.UnityUtils/Runtime/Csv/CsvRow.cs
+++ Assets/Uft.UnityUtils/Runtime/Csv/CsvRow.cs
@@ -16,7 +16,7 @@ namespace Uft.UnityUtils.Csv
         public string? GetString(int index) => index < 0 ? null : this.reader.GetField(index);
         public int GetInt32(int index) => index < 0 ? 0 : this.reader.GetField<int>(index);
         public float GetSingle(int index) => index < 0 ? 0 : this.reader.GetField<float>(index);
-        public bool GetBoolean(int index) => index < 0 ? false : this.reader.GetField<bool>(index);
+        public bool GetBoolean(int index) => index >= 0 && this.reader.GetField<bool>(index);
     }
 }
 #endif
