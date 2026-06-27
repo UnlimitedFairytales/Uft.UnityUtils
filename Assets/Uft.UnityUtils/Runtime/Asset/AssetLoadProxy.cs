583ad64932e174bfa007cca09115ac1eef82417f
SNOW\Ether <etherealgarden@msn.com>
2026-06-27 23:49:30 +0900

    AssetLoadProxy微調整

--- Assets/Uft.UnityUtils/Runtime/Asset/AssetLoadProxy.cs
+++ Assets/Uft.UnityUtils/Runtime/Asset/AssetLoadProxy.cs
@@ -15,16 +15,16 @@ namespace Uft.UnityUtils.Asset
             return Path.Combine(dirName, fileName).Replace(@"\", "/");
         }
 
-        readonly Func<string, Type, UnityEngine.Object>? _externalLoader;
+        readonly Func<string, Type, UnityEngine.Object?>? _externalLoader;
 
-        public AssetLoadProxy(Func<string, Type, UnityEngine.Object>? externalLoader) => this._externalLoader = externalLoader;
+        public AssetLoadProxy(Func<string, Type, UnityEngine.Object?>? externalLoader) => this._externalLoader = externalLoader;
 
         /// <summary>pathは拡張子付きで渡してOK。フォールバック時に <see cref="Resources.Load"/> する場合は除去される。見つからない場合は例外。</summary>
         public T Load<T>(string path) where T : UnityEngine.Object
         {
             if (this._externalLoader != null)
             {
-                var asset = (T)this._externalLoader(path, typeof(T));
+                var asset = (T?)this._externalLoader(path, typeof(T));
                 // NOTE: where制約 を認識しないため、抑制
 #pragma warning disable IDE0270 // coalesce 式を使用します
                 if (asset == null) throw new InvalidOperationException($"(External) Failed to load: {path}");
